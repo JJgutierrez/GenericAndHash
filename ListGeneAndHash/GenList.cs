d0bd53e9a35e6770ae5ec7e5d23e81f682915704
JJgutierrez <jjgues@live.com>
2016-01-06 16:47:51 -0600

    generic Try

---
+++ ListGeneAndHash/GenList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListGeneAndHash
+{
+   
+    public class GenericDictionary <TKey, TValue>
+    {
+        public void Add(TKey key, TValue value)
+        {
+
+        }
+    }
+    public class GenList<T>
+    {
+        public void Add(T value)
+        {
+
+        }
+        public T this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+    }
+}
---
+++ ListGeneAndHash/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListGeneAndHash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number:");
+            int num = int.Parse(Console.ReadLine());
+            var number = new Nullable<int>(num);
+
+            Console.WriteLine("Has a value ?"+ number.HasValue);
+            Console.WriteLine("Value"+ number.GetValueOrDefault());
+
+            Console.WriteLine("enter a number :");
+            int index = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter a Name :");
+            string name = Console.ReadLine();
+
+            var Nlist = new GenericDictionary<int,string>();
+            Nlist.Add(index, name);
+
+        }
+    }
+}
